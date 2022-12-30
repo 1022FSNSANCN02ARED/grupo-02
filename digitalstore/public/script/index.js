1a679129585fb98a4316b3222adc2f58ece91d74
Pablo Candela <pabloccandela@gmail.com>
2022-12-29 22:18:43 -0300

    menu de categorias desplegable para mobile sin arreglo en laptop

--- digitalstore/public/script/index.js
+++ digitalstore/public/script/index.js
@@ -4,3 +4,10 @@ const menuHam = document.querySelector(".nav-links");
 menuBtn.addEventListener("click", () => {
     menuHam.classList.toggle("show");
 });
+
+const catBtn = document.querySelector('.desplegable');
+const catHam = document.querySelector("#categorias-form");
+
+catBtn.addEventListener("click", () => {
+    catHam.classList.toggle("show-cat");
+});
