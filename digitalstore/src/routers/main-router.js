42113fa2cb7ea75d77879b882b0d3f0b47838696
Ben <rbenorlando@gmail.com>
2023-01-14 10:22:41 -0300

    header-main-footer-ejs

--- digitalstore/src/routers/main-router.js
+++ digitalstore/src/routers/main-router.js
@@ -7,7 +7,7 @@ router.get("/register", mainController.register);
 router.get("/login", mainController.login);
 router.get("/ayuda", mainController.ayuda);
 router.get("/contacto", mainController.contacto);
-router.get("/producto", mainController.producto);
+router.get("/productos", mainController.producto);
 router.get("/lista-producto", mainController.listaProducto);
 router.get("/carrito", mainController.carrito);
 module.exports = router;
