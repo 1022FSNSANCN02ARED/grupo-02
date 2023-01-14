5b7eb52c9d1a0ebc88b60c51fa75faae78396585
Ben <rbenorlando@gmail.com>
2023-01-14 10:01:38 -0300

    html-a-ejs

--- digitalstore/src/app.js
+++ digitalstore/src/app.js
@@ -19,37 +19,6 @@ app.use(express.static(path.join(__dirname,"../public")))
 //PLANTILLA EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
-//GETS
-//app.get("/",(req,res) => {
-//    res.sendFile(path.join(__dirname,"../views/index.html"))
-//})
-//app.get("/login",(req,res) => {
-//    res.sendFile(path.join(__dirname,"../views/login.html"))
-//})
-
-//app.get("/register", (req,res) => {
-//    res.sendFile(path.join(__dirname,"../views/register.html"))
-//})
-
-app.get("/carrito", (req,res) => {
-    res.sendFile(path.join(__dirname,"../views/carrito.html"))
-})
-
-/*se agrega pedido de respuesta a pagina de ayuda*/
-app.get("/ayuda", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/ayuda.html"));
-});
-
-/*se agrega pedido de respuesta a pagina de contacto*/
-app.get("/contacto", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/contacto.html"));
-});
-
-
-app.get("/productos", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/listadeproducto.html"));
-});
-
 
 //LISTEN 
 app.listen(PORT,()=> {
--- digitalstore/src/controllers/main-controller.js
+++ digitalstore/src/controllers/main-controller.js
@@ -7,5 +7,20 @@ module.exports = {
     },
     login: (req,res)=>{
         res.render("login")
+    },
+    contacto: (req,res)=>{
+        res.render("contacto")
+    },
+    ayuda: (req,res)=>{
+        res.render("ayuda")
+    },
+    carrito: (req,res)=>{
+        res.render("carrito")           
+    },
+    producto: (req,res)=>{
+        res.render("producto")
+    },
+    listaProducto: (req,res)=>{
+        res.render("listaProducto")
     }
 }
