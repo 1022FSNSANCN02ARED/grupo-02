eca4940869fd4861f35cb3b42b3ab6d97b92698b
Pablo Candela <pabloccandela@gmail.com>
2023-01-24 22:34:51 -0300

    modificacion en las rutas para añadir nuevo producto

--- digitalstore/src/routes/productsRoutes.js
+++ digitalstore/src/routes/productsRoutes.js
@@ -3,7 +3,7 @@ const router = Router();
 
 const productsController = require('../controllers/productsController');
 
-router.get("/:id", productsController.products);
+router.get("/details/:id", productsController.products);
 router.get("/", productsController.listProducts);
 
 router.get("/add", productsController.addProductsForm);
