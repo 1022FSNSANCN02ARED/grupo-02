46e2e8efe397d30adba603bc7e3135f6c11707e4
Pablo Candela <pabloccandela@gmail.com>
2023-01-25 21:47:49 -0300

    agregado de imagen default-image.png

--- digitalstore/src/controllers/productsController.js
+++ digitalstore/src/controllers/productsController.js
@@ -27,7 +27,7 @@ module.exports={
             nombre:req.body.nombre,
             precio:Number(req.body.precio),
             categoria:req.body.categoria,
-            image:req.file ? req.file.filename:"default-img.png",
+            image:req.file ? req.file.filename:"default-image.png",
             descripcion:req.body.descripcion,
             oferta:Number(req.body.descuento) != 0 ? true:false,
             descuento:Number(req.body.descuento)
