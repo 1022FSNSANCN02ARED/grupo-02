98da6487abbb1ae7e281a9ed23cbd720e788116b
Lautaro Pedroza <Lautimotoe99@gmail.com>
2023-03-28 19:46:42 -0300

    agregue la barra desplegable

--- digitalstore/public/script/header.js
+++ digitalstore/public/script/header.js
@@ -1,6 +1,30 @@
+window.addEventListener("load", function(){
 
-const user = document.querySelector(".usurio-header")
 
-user.addEventListener("click",()=>{
-    alert("hola")
+let usuario = document.querySelector(".usuarioHeader")
+let desplegable = document.querySelector(".div-desplegable-header")
+let cerrarDesplegable = document.querySelector(".cerrarDesplegable")
+
+
+usuario.addEventListener("click", function(e){
+    e.preventDefault();
+    desplegable.style.marginLeft = "0vw";
+    
+})
+cerrarDesplegable.addEventListener("click", function(e){
+    e.preventDefault();
+    desplegable.style.marginLeft = "60vw";
+    
+})
+
+
+
+
+
+
+
+
+
+
+
 })
