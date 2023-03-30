9584f46c92e59d490d4f6bc15b93c76c3f1912bf
Lautaro Pedroza <Lautimotoe99@gmail.com>
2023-03-30 18:06:55 -0300

    arregle el estilo del desplegable de usuario

--- digitalstore/public/script/header.js
+++ digitalstore/public/script/header.js
@@ -4,12 +4,15 @@ window.addEventListener("load", function(){
 let usuario = document.querySelector(".usuarioHeader")
 let desplegable = document.querySelector(".div-desplegable-header")
 let cerrarDesplegable = document.querySelector(".cerrarDesplegable")
+let body = document.querySelector("body")
 
 
 usuario.addEventListener("click", function(e){
     e.preventDefault();
     desplegable.style.right = "0px";
     desplegable.style.transition = "right 0.2s linear 0.2s"
+    
+    
     
 })
 cerrarDesplegable.addEventListener("click", function(e){
