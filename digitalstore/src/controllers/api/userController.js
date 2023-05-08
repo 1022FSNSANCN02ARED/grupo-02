a87ed0d989e1fa2851615927e2928a7494ac32ae
Ben <rbenorlando@gmail.com>
2023-05-08 18:23:47 -0300

    ajuste-api

--- digitalstore/src/controllers/api/userController.js
+++ digitalstore/src/controllers/api/userController.js
@@ -27,5 +27,110 @@ module.exports= {
             },
             data: user
         });
-    }
+    },
+    //***De aca para abajo a revisar**// 
+    create: (req,res)=>{
+        User.create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userName: req.body.userName,
+            password: req.body.password,
+            email: req.body.email,
+            img: req.body.img,
+            idRole: req.body.id_role
+
+        })
+        .then((confirm)=>{
+            let respuesta;
+            if(confirm){
+                respuesta = {
+                    meta: {
+                        status:!confirm ? 404 : 200,
+                        total: confirm.length,
+                        url: "api/users/create",
+                    },
+                    data: confirm,
+                }
+            } else {
+                respuesta = {
+                    meta: {
+                        status: !confirm ? 404 : 200,
+                        total: confirm.length,
+                        url:"api/users/create",
+                    },
+                    data: confirm,
+                }
+            }
+            res.json(respuesta);
+        })
+        .catch((error)=> res.send(error));
+    },
+    update: (req,res)=>{
+        let userId = req.params.id;
+        User.update({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userName: req.body.userName,
+            password: req.body.password,
+            email: req.body.email,
+            img: req.body.img,
+            idRole: req.body.id_role
+        },{
+            where: { id: userId },
+        })
+        .then((confirm)=>{
+            let respuesta;
+            if (confirm) {
+                respuesta = {
+                  meta: {
+                    status: 200,
+                    total: confirm.length,
+                    url: "api/users/:id",
+                  },
+                  data: confirm,
+                };
+            } else {
+                respuesta = {
+                    meta: {
+                        status: 204,
+                        total: confirm.length,
+                        url: "api/users/:id",
+                    },
+                    data: confirm,
+                };
+            }
+            res.json(respuesta);    
+        })
+        .catch((error)=>res.send(error));
+    },
+    destroy: (req,res)=>{
+        let userId = req.params.id;
+        User.destroy({where: {id:userId}, force: true})
+        .then((confirm)=>{
+            let respuesta;
+            if (confirm) {
+                respuesta = {
+                  meta: {
+                    status: 200,
+                    total: confirm.length,
+                    url: "api/users/delete/:id",
+                  },
+                  data: confirm,
+                };
+            } else {
+                respuesta = {
+                  meta: {
+                    status: 204,
+                    total: confirm.length,
+                    url: "api/users/destroy/:id",
+                  },
+                  data: confirm,
+                };
+              }
+              res.json(respuesta);
+            })
+            .catch((error) => res.send(error));
+    },
+        
+    
 }
