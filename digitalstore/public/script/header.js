2dc91f031ca2dd6c84565e3049b65a9d688746f6
Pabloccandela <pabloccandela@gmail.com>
2023-05-14 17:06:45 -0300

    cambios finalizacion de la barra de navegacion (responsive) y arreglo de estilo en la seccion de ayuda

--- digitalstore/public/script/header.js
+++ digitalstore/public/script/header.js
@@ -1,44 +1,12 @@
 window.addEventListener("load", function(){
-
-
-let usuario = document.querySelector(".usuarioHeader")
-let desplegable = document.querySelector(".div-desplegable-header")
-let cerrarDesplegable = document.querySelector(".cerrarDesplegable")
-let body = document.querySelector("body")
-let cerrarMobile = document.querySelector(".cerrar-desplegable")
-
-
-usuario.addEventListener("click", function(e){
-    e.preventDefault();
-    desplegable.style.right = "0px";
-    desplegable.style.transition = "right 0.2s linear 0.2s";
-})
-
-desplegable.addEventListener("mouseover", function(e){
-    e.preventDefault();
-    desplegable.style.right = "0px";
-      
-})
-
-desplegable.addEventListener("mouseout", function(e){
-    e.preventDefault();
-    desplegable.style.right = "-400px";
     
-})
+    const desplegable = document.querySelector(".nav-ul");
+    const btnDesplegable = document.querySelector(".phone-icon-menu");
+    const body = document.querySelector("body");
 
-cerrarMobile.addEventListener("click", function(){
-    desplegable.style.right = "-400px";
-    
-} )
-
-
-
-
-
-
-
-
-
-
+    btnDesplegable.addEventListener("click",() => { 
+        desplegable.classList.toggle("open-nav");
+        body.classList.toggle("overflow-hidden");
+    })
 
 })
