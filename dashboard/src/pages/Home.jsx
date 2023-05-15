550ed1ca22d0776cb27ad1ad057dde4f498919bd
Melina Leonardi <melina61285@gmail.com>
2023-05-15 18:35:44 -0300

    vista home

--- dashboard/src/pages/Home.jsx
+++ dashboard/src/pages/Home.jsx
@@ -3,25 +3,57 @@ import SmallCard from "../components/SmallCard";
 import LastProduct from "../components/LastProduct";
 import LastUser from "../components/LastUser";
 import "./Home.css";
-const myStats = [
-  {
-    id: 1,
-    title: "Productos en DB",
-    value: "19",
-  },
-  {
-    id: 2,
-    title: "Total de Marcas",
-    value: "10",
-  },
-  {
-    id: 3,
-    title: "Total de Categorias",
-    value: "8",
-  },
-];
+
+// const myStats = [
+//   {
+//     id: 1,
+//     title: "Productos en DB",
+//     value: 0,
+//   },
+//   {
+//     id: 2,
+//     title: "Total de Marcas",
+//     value: 0,
+//   },
+//   {
+//     id: 3,
+//     title: "Total de Categorias",
+//     value: 0,
+//   },
+// ];
 
 function Home() {
+  const [productsTotal, setproductsTotal] = useState(0);
+  const [brandsTotal, setbrandsTotal] = useState(0);
+  const [categoryTotal, setcategoryTotal] = useState(0);
+
+  function apiProduct() {
+    fetch(link)
+      .then((response) => response.json())
+      .then((data) => {
+        myStats[pos].value = data.meta.total;
+      })
+      .catch((error) => console.error(error));
+  }
+
+  function apiBrand() {
+    fetch(link)
+      .then((response) => response.json())
+      .then((data) => {
+        myStats.value = data.meta.total;
+      })
+      .catch((error) => console.error(error));
+  }
+
+  function apiCategory() {
+    fetch()
+      .then((response) => response.json())
+      .then((data) => {
+        myStats[pos].value = data.meta.total;
+      })
+      .catch((error) => console.error(error));
+  }
+
   return (
     <>
       <div className="indicadores">
