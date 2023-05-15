1501a5098e9c960711f92792853d9067ed5a1afe
Pabloccandela <pabloccandela@gmail.com>
2023-05-15 20:13:47 -0300

    arreglo de desplegable de filtros

--- digitalstore/public/script/index.js
+++ digitalstore/public/script/index.js
@@ -6,14 +6,19 @@ const user = document.querySelector(".usuario.header")
 const catBtn = document.querySelector('.desplegable');
 const catHam = document.querySelector("#categorias-form");
 
-// catBtn.addEventListener("click", () => {
-//     catHam.classList.toggle("show-cat");
-// })
-// ;
+catBtn.addEventListener("click", () => {
+    catHam.classList.toggle("show-cat");
+})
+;
 
 const products = document.getElementById("product-item");
 products.addEventListener("click",(e) => {
     window.location.href = "/products";
 })
+
+// const filters = document.querySelector(".container-categories form");
+// const filtersBtn = document.querySelector(".desplegable");
+
+
 
 
