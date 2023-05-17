10571ccf46b1bed0fe673722d1025b572637c59f
Lautaro Pedroza <lautimotoe99@gmail.com>
2023-05-17 12:56:14 -0300

    asd

--- dashboard/src/components/BigCard.jsx
+++ dashboard/src/components/BigCard.jsx
@@ -4,11 +4,14 @@ import "./BigCard.css";
 function BigCard({ title, children }) {
   return (
     <>
+    <div className="bigcard">
       <div className="idCard">
         <h3>{title}</h3>
       </div>
 
       <div className="descCard">{children}</div>
+    </div>
+      
     </>
   );
 }
