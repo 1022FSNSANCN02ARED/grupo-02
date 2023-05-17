fa7ac497f55bd4142ee4cf361f5b2c53872a6b48
Pabloccandela <pabloccandela@gmail.com>
2023-05-16 22:42:17 -0300

    terminado de estilos para el detalle de producto

---
+++ digitalstore/public/script/productDetail.js
@@ -0,0 +1,5 @@
+const buybutton = document.querySelector(".buy-button p")
+
+buybutton.addEventListener("click",() => { 
+    this.alert("La página se encuentra en mantenimiento, pronto la abriremos al publico este atento!!!")
+})
